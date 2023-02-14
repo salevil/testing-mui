761f8f66941d63a6af91c04ba2b9fb5e79c7cd3a
Stev Levillain <salevillain@gmail.com>
2023-02-14 09:27:55 -0600

    other things

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import MyButton from "./myButton";
 
 
@@ -13,12 +12,10 @@ function Header({count, handleClick}) {
     <React.Fragment>
       <AppBar>
         <Toolbar>
-            <Typography color="inherit" variant="h5" component="h1">
+              <Typography color="inherit" variant="h5" component="h1">
                 Testing MUI
             </Typography>
-            <Grid container justify="flex-end">
               <MyButton count={count} onClick={handleClick} />
-            </Grid>
         </Toolbar>
       </AppBar>
     </React.Fragment>
