68cfa98391cfe02676028eee2f981012fd0fd975
Stev Levillain <salevillain@gmail.com>
2023-04-28 15:02:01 -0500

    template for addFoV

--- src/components/addFruitOrVeggies.js
+++ src/components/addFruitOrVeggies.js
@@ -11,3 +11,33 @@ export default function AddFruitOrVeggies({value, handleChange}) {
         />
     )
 }
+
+// import { useState } from 'react';
+// import { TextField, FormControlLabel, Checkbox, Button } from '@mui/material';
+
+// export default function MyForm() {
+//   const [name, setName] = useState('');
+//   const [isFruit, setIsFruit] = useState(false);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log({ name, isFruit });
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <TextField
+//         label="Name"
+//         value={name}
+//         onChange={(event) => setName(event.target.value)}
+//         fullWidth
+//         margin="normal"
+//       />
+//       <FormControlLabel
+//         control={<Checkbox checked={isFruit} onChange={(event) => setIsFruit(event.target.checked)} />}
+//         label="Is Fruit"
+//       />
+//       <Button type="submit">Submit</Button>
+//     </form>
+//   );
+// }
