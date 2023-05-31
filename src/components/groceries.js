c610a2dde6ebc42b5b816372d746959f54d0dde2
Stev Levillain <salevillain@gmail.com>
2023-05-31 14:42:19 -0500

    fixed groceries.js

--- src/components/groceries.js
+++ src/components/groceries.js
@@ -1,27 +1,24 @@
-export default function Groceries({groceries}) {
+export default function Groceries({ groceries }) {
     return (
-        <ul>
-            {groceries.map((item) => (
-                <li
-                    key={item.id}
-                    style={{
-                        if (item.category === "house supplies") {
-                            color: 'magenta'
-                        // }  
-                        // else if(item.category === "produce") {
-                        //     color: 'darkgreen'
-                        // }
-                        // else if(item.category === "dairy") {
-                        //     color: 'blue'
-                        // }
-                        // else if(item.category === "meat") {
-                        //     color: 'orange'
-                        }
-                    }} 
-                    >
-                       {item.name} 
-                </li>
-            ))}
-        </ul>
-        )
-}
+      <ul>
+        {groceries.map((item) => {
+          let color;
+          if (item.category === "house supplies") {
+            color = 'magenta';
+          } else if (item.category === "produce") {
+            color = 'darkgreen';
+          } else if (item.category === "dairy") {
+            color = 'blue';
+          } else if (item.category === "meat") {
+            color = 'orange';
+          }
+  
+          return (
+            <li key={item.id} style={{ color }}>
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
