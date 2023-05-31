7b379828a8d034a72a8990728499c9cac863dbb4
Stev Levillain <stev.levillain@charter.com>
2023-05-31 16:24:15 -0500

    issue with adding items

--- src/components/groceries/addItems.js
+++ src/components/groceries/addItems.js
@@ -9,7 +9,7 @@ import {
   Button
 } from '@mui/material';
 
-export default function AddItems({onGroceriesSubmit}) {
+export default function AddItems({onGroceriesSubmit, categories}) {
   const [item, setItem] = useState('');
   const [category, setCategory] = useState(false);
 
@@ -36,14 +36,15 @@ export default function AddItems({onGroceriesSubmit}) {
       />
         <FormControl>
           <FormLabel>Category</FormLabel>
-          <RadioGroup value={category} onChange={(event) => setCategory(event.target.value)} >
-            {categories.map(category) => {
-              
-            }
-            <FormControlLabel value="produce" control={<Radio />} label="produce"/>
-            <FormControlLabel value="dairy" control={<Radio />} label="dairy"/>
-            <FormControlLabel value="meat" control={<Radio />} label="meat"/>
-            <FormControlLabel value="house supplies" control={<Radio />} label="house supplies"/>
+          <RadioGroup value={category} onChange={(event) => setCategory(event.target.value)}>
+            {categories.map((category) => (
+              <FormControlLabel
+                key={category}
+                value={category}
+                control={<Radio />}
+                label={category}
+              />
+            ))}
           </RadioGroup>
         </FormControl>
       <Button type="submit" variant="contained">Submit</Button>
